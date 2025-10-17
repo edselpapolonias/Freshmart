341c3b419bd4054e408669094b94238d6b030611
edselpapolonias <edselpapolonias1@gmail.com>
2025-10-17 14:03:01 +0800

    Database MYSQL

--- freshmart_project/freshmart_project/settings.py
+++ freshmart_project/freshmart_project/settings.py
@@ -78,8 +78,15 @@ WSGI_APPLICATION = 'freshmart_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'freshmart_ims_db',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
